144e8cbf266031ded68200af752f61b748cde807
zggff <gigamaximwachau@gmail.com>
2024-07-06 17:05:26 +0300

    represent enum as string

--- src/User.cs
+++ src/User.cs
@@ -1,6 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Zggff.MaiPractice;
 
@@ -10,12 +11,13 @@ public class User
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public uint Id { get; set; }
     [Required(ErrorMessage = "Login is required")]
-    public string? Login { get; set; }
+    public required string Login { get; set; }
     [Required(ErrorMessage = "Password is required")]
-    public string? Password { get; set; }
+    public required string Password { get; set; }
     public Role Role { get; set; }
 }
 
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum Role
 {
     User,
